1e57336bdc8a4cbc8cd0518b6bf1965ddddcd409
Danielle Kom <daniellekom1@gmail.com>
2022-04-25 11:32:42 -0400

    created routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
-import 'pages/home.dart';
+import 'package:world_app/pages/choose_location.dart';
+import 'package:world_app/pages/loading.dart';
+import 'package:world_app/pages/home.dart';
 
 void main() =>runApp(MaterialApp(
-home: Home(),
+initialRoute: '/home',
+routes: {
+  '/':(context) => Loading(),
+  '/home': (context) => Home(),
+  '/location':(context) => ChooseLocation()
+},
 ));
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,8 +10,20 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-     body: SafeArea(child: Text('home screen')),
+    return Scaffold(
+     body: SafeArea(
+       child:Column(
+         children: [
+           TextButton.icon(
+             onPressed:() {
+               Navigator.pushNamed(context, '/location');
+             },
+              icon: const Icon(Icons.edit_location_alt),
+               label: const Text('Edit Location'),
+           ),
+         ],
+       ),
+     ),
     );
   }
 }
