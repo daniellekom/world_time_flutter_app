82c13edad7d6deb526a59d35f2a26fdad5ac6dc0
Danielle Kom <daniellekom1@gmail.com>
2022-04-25 11:04:15 -0400

    creating routes with dart

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+
+  @override
+  _ChooseLocationState createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('choose location screen'),
+      
+    );
+  }
+}
